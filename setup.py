59f0e1e473f66d12a44bb14da408c8e29d8b57e6
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-11-09 16:04:08 +0000

    Log the client out when an unauthorised response is received

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-moj-auth',
-    version='0.3',
+    version='0.4',
     packages=['moj_auth', 'moj_auth.tests'],
     include_package_data=True,
     license='BSD License',
