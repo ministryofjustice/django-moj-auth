0b001d31c7a76fa4814e0b7d4f9af2958c76e792
Ian Brechin <ian.brechin@gmail.com>
2015-12-14 17:14:48 +0000

    Increase version number to 0.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-moj-auth',
-    version='0.5',
+    version='0.6',
     packages=['moj_auth', 'moj_auth.tests'],
     include_package_data=True,
     license='BSD License',
